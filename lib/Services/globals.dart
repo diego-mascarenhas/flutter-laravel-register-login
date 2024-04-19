8fbf7479a2b2abd1d45757c52ace64a299f16c3d
Diego Mascarenhas <diego@revisionalpha.com>
2024-04-19 17:13:22 +0200

    first commit

---
+++ lib/Services/globals.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+//const String baseURL = "http://127.0.0.1:8000/api/";
+const String baseURL = "https://cms8.revisionalpha.es/api/";
+const Map<String, String> headers = {"Content-Type": "application/json"};
+
+errorSnackBar(BuildContext context, String text) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    backgroundColor: Colors.red,
+    content: Text(text),
+    duration: const Duration(seconds: 1),
+  ));
+}
